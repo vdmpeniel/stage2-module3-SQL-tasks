8a31f0375c1c262bb6a1c4699134b3ee3ae875ea
victor diaz <vdmpeniel@yahoo.es>
2023-08-09 13:07:18 -0400

    Made all selects required by lesson 4 tasks.

--- lesson4/src/main/resources/SOLUTION.sql
+++ lesson4/src/main/resources/SOLUTION.sql
@@ -1,6 +1,14 @@
 -- Lesson 4 tasks:
 
 -- 1. Select all students with all the available fields
+SELECT * FROM Student;
+
 -- 2. Select 50 students
+SELECT * FROM Student LIMIT 50;
+
 -- 3. Select only students’ name
+SELECT name FROM Student;
+
 -- 4. Select unique values of Amount from Payment table
+SELECT DISTINCT amount FROM Payment;
+
