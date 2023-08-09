a092ed7f6a04d49d5ddcfcf5806b9c7fcbfe0d32
victor diaz <vdmpeniel@yahoo.es>
2023-08-09 11:16:31 -0400

    Added all data required by lesson 2

--- lesson1/src/main/resources/SOLUTION.sql
+++ lesson1/src/main/resources/SOLUTION.sql
@@ -1,2 +1,51 @@
-CREATE DATABASE University
+#CREATE DATABASE University;
+
+CREATE TABLE Student
+(
+    id bigint NOT NULL UNIQUE AUTO_INCREMENT,
+    name varchar(255) NOT NULL,
+    birthday date NOT NULL,
+    group_number int NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Subject
+(
+    id bigint NOT NULL UNIQUE AUTO_INCREMENT,
+    name varchar(255) NOT NULL,
+    description varchar(255),
+    grade int,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE PaymentType
+(
+    id bigint NOT NULL UNIQUE AUTO_INCREMENT,
+    name varchar(255),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Payment
+(
+    id bigint NOT NULL UNIQUE AUTO_INCREMENT,
+    type_id bigint NOT NULL,
+    amount decimal NOT NUL,
+    student_id bigint NOT NULL,
+    payment_date datetime NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (student_id) REFERENCES Student (id),
+    FOREIGN KEY (type_id) REFERENCES PaymentType (id)
+);
+
+CREATE TABLE Mark
+(
+    id bigint NOT NULL UNIQUE AUTO_INCREMENT,
+    student_id bigint NOT NULL,
+    subject_id bigint NOT NULL,
+    mark int  NOT NULL,
+    PRIMARY KEY (ID),
+    FOREIGN KEY (student_id) REFERENCES Student (id),
+    FOREIGN KEY (subject_id) REFERENCES Subject (id)
+);
+
 
--- lesson2/src/main/resources/SOLUTION.sql
+++ lesson2/src/main/resources/SOLUTION.sql
@@ -1,47 +1,125 @@
-CREATE TABLE Student
-(
-    id bigint NOT NULL UNIQUE AUTO_INCREMENT,
-    name varchar(255) NOT NULL,
-    birthday date NOT NULL,
-    group_number int NOT NULL,
-    PRIMARY KEY (id)
-);
+-- Lesson 2
+-- 1) Adding new students:
+-- First grade
+INSERT INTO Student(
+    name,
+    birthday,
+    group_number
+)
+VALUES
+    ('John', '2018-02-21', 1),
+    ('Chris', '2018-02-21', 1),
+    ('Carl', '2018-02-21', 1);
 
-CREATE TABLE Subject
-(
-    id bigint NOT NULL UNIQUE AUTO_INCREMENT,
-    name varchar(255) NOT NULL,
-    description varchar(255),
-    grade int,
-    PRIMARY KEY (id)
-);
 
-CREATE TABLE PaymentType
-(
-    id bigint NOT NULL UNIQUE AUTO_INCREMENT,
-    name varchar(255),
-    PRIMARY KEY (id)
-);
+-- Second grade
+INSERT INTO Student(
+    name,
+    birthday,
+    group_number
+)
+VALUES
+    ('Oliver', '2018-02-21', 2),
+    ('James', '2018-02-21', 2),
+    ('Lucas', '2018-02-21', 2),
+    ('Henry', '2018-02-21', 2);
 
-CREATE TABLE Payment
-(
-    id bigint NOT NULL UNIQUE AUTO_INCREMENT,
-    type_id bigint NOT NULL,
-    amount decimal NOT NUL,
-    student_id bigint NOT NULL,
-    payment_date datetime NOT NULL,
-    PRIMARY KEY (ID),
-    FOREIGN KEY (student_id) REFERENCES Student (id),
-    FOREIGN KEY (type_id) REFERENCES PaymentType (id)
-);
 
-CREATE TABLE Mark
-(
-    id bigint NOT NULL UNIQUE AUTO_INCREMENT,
-    student_id bigint NOT NULL,
-    subject_id bigint NOT NULL,
-    mark int  NOT NULL,
-    PRIMARY KEY (ID),
-    FOREIGN KEY (student_id) REFERENCES Student (id),
-    FOREIGN KEY (subject_id) REFERENCES Subject (id)
-);
+-- Third grade
+INSERT INTO Student(
+    name,
+    birthday,
+    group_number
+)
+VALUES
+    ('Jacob', '2018-02-21', 3),
+    ('Logan', '2018-02-21', 3);
+
+
+-- Other grades
+INSERT INTO Student(
+    name,
+    birthday,
+    group_number
+)
+VALUES
+    ('William', '2018-02-21', 4),
+    ('Julia', '2018-02-21', 4),
+    ('Anselm', '2018-02-21', 5);
+-- SELECT * FROM Student;
+
+
+-- 2) Adding new Subjects:
+INSERT INTO Subject(
+    name,
+    description,
+    grade
+)
+VALUES
+('Art', '', 1),
+('Music', '', 1),
+
+('Geography', '', 2),
+('History', '', 2),
+
+('PE', '', 3),
+('Math', '', 3),
+
+('Science', '', 4),
+('IT', '', 4),
+
+('Computer Science', '', 5),
+('Nuclear Physics', '', 5);
+-- SELECT * FROM Subject;
+
+
+-- 3) Insert “DAILY”, “WEEKLY”, ”MONTHLY” Payment Types.
+INSERT INTO PaymentType(id, name)
+VALUES
+('DAILY'),
+('WEEKLY'),
+('MONTHLY');
+
+
+-- 4) Insert 4 Payments:
+INSERT INTO Payment (
+    type_id,
+    amount,
+    student_id,
+    payment_date
+)
+VALUES
+    ((SELECT id FROM PaymentType WHERE name = 'WEEKLY'), 25, (SELECT id FROM Student WHERE name = 'John'), CURDATE()),
+    ((SELECT id FROM PaymentType WHERE name = 'MONTHLY'), 250, (SELECT id FROM Student WHERE name = 'Oliver'), CURDATE()),
+    ((SELECT id FROM PaymentType WHERE name = 'WEEKLY'), 25, (SELECT id FROM Student WHERE name = 'Henry'), CURDATE()),
+    ((SELECT id FROM PaymentType WHERE name = 'DAILY'), 5, (SELECT id FROM Student WHERE name = 'James'), CURDATE()),
+    ((SELECT id FROM PaymentType WHERE name = 'WEEKLY'), 20, (SELECT id FROM Student WHERE name = 'Julia'), CURDATE()),
+    ((SELECT id FROM PaymentType WHERE name = 'DAILY'), 45, (SELECT id FROM Student WHERE name = 'Anselm'), CURDATE()),
+    ((SELECT id FROM PaymentType WHERE name = 'MONTHLY'), 120, (SELECT id FROM Student WHERE name = 'Jacob'), CURDATE()),
+    ((SELECT id FROM PaymentType WHERE name = 'DAILY'), 15, (SELECT id FROM Student WHERE name = 'Logan'), CURDATE());
+-- SELECT * FROM Payment;
+
+
+
+-- 5) Insert 5 Marks:
+- 8 for Chris by Art;
+- 5 for Oliver by History;
+- 9 for James by Geography;
+- 4 for Jacob by Math;
+- 9 for Logan by PE.
+- Insert more Masks for students.
+
+INSERT INTO Mark(
+    student_id,
+    subject_id,
+    mark
+)
+VALUES
+((SELECT id FROM Student WHERE name = 'Chris'), (SELECT id FROM Subject WHERE name = 'Art'), 8),
+((SELECT id FROM Student WHERE name = 'Oliver'), (SELECT id FROM Subject WHERE name = 'History'), 5),
+((SELECT id FROM Student WHERE name = 'James'), (SELECT id FROM Subject WHERE name = 'Geography'), 9),
+((SELECT id FROM Student WHERE name = 'Jacob'), (SELECT id FROM Subject WHERE name = 'Math'), 4),
+((SELECT id FROM Student WHERE name = 'Logan'), (SELECT id FROM Subject WHERE name = 'PE'), 9),
+((SELECT id FROM Student WHERE name = 'Lucas'), (SELECT id FROM Subject WHERE name = 'Math'), 10),
+((SELECT id FROM Student WHERE name = 'Julia'), (SELECT id FROM Subject WHERE name = 'Art'), 7);
+-- SELECT * FROM Mark;
