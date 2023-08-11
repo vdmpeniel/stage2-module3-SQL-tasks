0a6106eb6cdcf03159120222e7418de85cdfc00b
victor diaz <vdmpeniel@yahoo.es>
2023-08-11 13:49:51 -0400

    Made all sorted selections required by lesson 7's tasks.

---
+++ lesson7/src/main/resources/SOLUTION.sql
@@ -0,0 +1,27 @@
+-- ## Lesson 7 tasks:
+
+
+-- 1) Select marks which is more than 6 and filter in descending order;
+SELECT * FROM Mark
+WHERE mark > 6
+ORDER BY mark DESC;
+
+-- 2) Select payments which is less than 300 and filter in ascending order;
+SELECT * FROM Payment
+WHERE amount < 300
+ORDER BY amount ASC;
+
+-- 3) Select payment types filtered alphabetically;
+SELECT * FROM PaymentType
+ORDER BY name ASC;
+
+-- 4) Select students filtered in reverse alphabetical order;
+SELECT * FROM Student
+ORDER BY nae DESC;
+
+-- 5) Select students who have at least one payment more than 1000 and filter them by birthday date ASC.
+SELECT DISTINCT st.* FROM Student AS st
+JOIN Payment AS p
+ON st.id = p.student_id
+WHERE p.amount > 1000
+ORDER BY st.birthday ASC;
