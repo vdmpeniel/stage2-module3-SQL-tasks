c0973acf03f91f973d8eabed5bfb505194b5f085
victor diaz <vdmpeniel@yahoo.es>
2023-08-12 01:00:58 -0400

    Fixing test errors - 2nd Iteration.

--- lesson10/src/main/resources/SOLUTION.sql
+++ lesson10/src/main/resources/SOLUTION.sql
@@ -1,3 +1,3 @@
 SELECT s.* FROM Subject AS s INNER JOIN Mark AS m ON m.subject_id = s.id GROUP BY s.id HAVING AVG(m.mark) > ( SELECT AVG(mark) FROM Mark);
 
-SELECT s.* FROM Student AS s INNER JOIN Payment AS p ON s.id = p.student_id GROUP BY s.id HAVING SUM(p.amount) > (SELECT AVG(amount) FROM Payment);
+SELECT DISTINCT s.* FROM Student AS s INNER JOIN Payment AS p ON s.id = p.student_id WHERE p.amount > (SELECT AVG(amount) FROM Payment);
