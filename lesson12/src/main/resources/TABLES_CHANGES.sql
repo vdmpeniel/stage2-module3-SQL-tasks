a441092a308a2f8caee7fa2670b90187c3c3c3ca
victor diaz <vdmpeniel@yahoo.es>
2023-08-15 19:16:49 -0400

    Test CREATE.

--- lesson12/src/main/resources/TABLES_CHANGES.sql
+++ lesson12/src/main/resources/TABLES_CHANGES.sql
@@ -1,11 +1,9 @@
-ALTER TABLE mark DROP CONSTRAINT fk_mark_student;
-ALTER TABLE mark ADD CONSTRAINT fk_mark_student FOREIGN KEY (student_id) REFERENCES student(id) ON DELETE CASCADE;
 
-ALTER TABLE mark DROP CONSTRAINT fk_mark_subject;
-ALTER TABLE mark ADD CONSTRAINT fk_mark_subject FOREIGN KEY (subject_id) REFERENCES subject(id) ON DELETE CASCADE;
+ALTER TABLE Mark DROP CONSTRAINT mark_student_id_fkey;
+ALTER TABLE Mark ADD CONSTRAINT mark_student_id_fkey FOREIGN KEY (student_id) REFERENCES Student(id) ON DELETE CASCADE;
 
-ALTER TABLE payment DROP CONSTRAINT fk_payment_type;
-ALTER TABLE payment ADD CONSTRAINT fk_payment_type FOREIGN KEY (type_id) REFERENCES paymentType(id) ON DELETE CASCADE;
+ALTER TABLE Payment DROP CONSTRAINT payment_student_id_fkey;
+ALTER TABLE Payment ADD CONSTRAINT payment_student_id_fkey FOREIGN KEY (student_id) REFERENCES Student(id) ON DELETE CASCADE;
 
-ALTER TABLE payment DROP CONSTRAINT fk_payment_student;
-ALTER TABLE payment ADD CONSTRAINT fk_payment_student FOREIGN KEY (student_id) REFERENCES student(id) ON DELETE CASCADE;
+ALTER TABLE Payment DROP CONSTRAINT payment_type_id_fkey;
+ALTER TABLE Payment ADD CONSTRAINT payment_type_id_fkey FOREIGN KEY (type_id) REFERENCES PaymentType(id) ON DELETE CASCADE;
--- lesson2/src/main/resources/CREATE.sql
+++ lesson2/src/main/resources/CREATE.sql
@@ -1,51 +1,43 @@
 
-CREATE TABLE student
-(
-    id BIGINT AUTO_INCREMENT NOT NULL,
+CREATE TABLE Student(
+    id BIGINT NOT NULL AUTO_INCREMENT,
     name VARCHAR NOT NULL,
     birthday DATE NOT NULL,
     groupnumber INT NOT NULL,
-    PRIMARY KEY(id)
+    PRIMARY KEY( id )
 );
-CREATE TABLE subject
-(
-    id BIGINT AUTO_INCREMENT NOT NULL,
+
+CREATE TABLE Subject(
+    id BIGINT NOT NULL AUTO_INCREMENT,
     name VARCHAR NOT NULL,
     description VARCHAR NOT NULL,
     grade INT NOT NULL,
-    PRIMARY KEY(id)
+    PRIMARY KEY( id )
 );
 
-CREATE TABLE mark
-(
-    id BIGINT AUTO_INCREMENT NOT NULL,
+CREATE TABLE PaymentType(
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR NOT NULL,
+    PRIMARY KEY( id )
+);
+
+CREATE TABLE Payment(
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    type_id BIGINT NOT NULL,
+    amount DECIMAL NOT NULL,
+    student_id BIGINT NOT NULL,
+    payment_date TIMESTAMP NOT NULL,
+    PRIMARY KEY( id )
+);
+ALTER TABLE Payment ADD CONSTRAINT PaymentStudentFk FOREIGN KEY (student_id) REFERENCES Student(id);
+ALTER TABLE Payment ADD CONSTRAINT PaymentPaymentTypeFk FOREIGN KEY (type_id) REFERENCES PaymentType(id);
+
+CREATE TABLE Mark(
+    id BIGINT NOT NULL AUTO_INCREMENT,
     student_id BIGINT NOT NULL,
     subject_id BIGINT NOT NULL,
     mark INT NOT NULL,
-    PRIMARY KEY(id)
+    PRIMARY KEY( id )
 );
-ALTER TABLE mark ADD CONSTRAINT fk_mark_student FOREIGN KEY (student_id) REFERENCES student(id);
-ALTER TABLE mark ADD CONSTRAINT fk_mark_subject FOREIGN KEY (subject_id) REFERENCES subject(id);
-
-
-
-
-CREATE TABLE paymenttype
-(
-    id BIGINT AUTO_INCREMENT NOT NULL,
-    name VARCHAR NOT NULL,
-    PRIMARY KEY(id)
-);
-
-CREATE TABLE payment
-(
-    id BIGINT AUTO_INCREMENT NOT NULL,
-    type_id BIGINT NOT NULL,
-    amount DECIMAL NOT NULL,
-    payment_date DATETIME,
-    student_id BIGINT NOT NULL,
-    PRIMARY KEY(id)
-);
-ALTER TABLE Payment ADD CONSTRAINT fk_payment_type FOREIGN KEY (type_id) REFERENCES paymentType(id);
-ALTER TABLE Payment ADD CONSTRAINT fk_payment_student FOREIGN KEY (student_id) REFERENCES student(id);
-
+ALTER TABLE Mark ADD CONSTRAINT MarkStudentFk FOREIGN KEY (student_id) REFERENCES Student(id);
+ALTER TABLE Mark ADD CONSTRAINT MarkSubjectFk FOREIGN KEY (subject_id) REFERENCES Subject(id);
