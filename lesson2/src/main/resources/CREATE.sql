7b227208d800e72265ac4863dcdf8bc7de21f9c7
victor diaz <vdmpeniel@yahoo.es>
2023-08-15 18:55:31 -0400

    Working on CREATE issues #2.

--- lesson2/src/main/resources/CREATE.sql
+++ lesson2/src/main/resources/CREATE.sql
@@ -1,27 +1,27 @@
 
 CREATE TABLE student
 (
-    id BIGINT UNIQUE AUTO_INCREMENT NOT NULL,
-    name VARCHAR(255),
-    birthday DATE,
-    groupnumber INT,
+    id BIGINT AUTO_INCREMENT NOT NULL,
+    name VARCHAR NOT NULL,
+    birthday DATE NOT NULL,
+    groupnumber INT NOT NULL,
     PRIMARY KEY(id)
 );
 CREATE TABLE subject
 (
-    id BIGINT UNIQUE AUTO_INCREMENT NOT NULL,
-    name VARCHAR(255),
-    description VARCHAR(255),
+    id BIGINT AUTO_INCREMENT NOT NULL,
+    name VARCHAR NOT NULL,
+    description VARCHAR NOT NULL,
     grade INT NOT NULL,
     PRIMARY KEY(id)
 );
 
 CREATE TABLE mark
 (
-    id BIGINT UNIQUE AUTO_INCREMENT NOT NULL,
-    student_id BIGINT,
-    subject_id BIGINT,
-    mark INT,
+    id BIGINT AUTO_INCREMENT NOT NULL,
+    student_id BIGINT NOT NULL,
+    subject_id BIGINT NOT NULL,
+    mark INT NOT NULL,
     PRIMARY KEY(id)
 );
 ALTER TABLE mark ADD CONSTRAINT fk_mark_student FOREIGN KEY (student_id) REFERENCES student(id);
@@ -32,18 +32,18 @@ ALTER TABLE mark ADD CONSTRAINT fk_mark_subject FOREIGN KEY (subject_id) REFEREN
 
 CREATE TABLE paymenttype
 (
-    id BIGINT UNIQUE AUTO_INCREMENT NOT NULL,
-    name VARCHAR(255),
+    id BIGINT AUTO_INCREMENT NOT NULL,
+    name VARCHAR NOT NULL,
     PRIMARY KEY(id)
 );
 
 CREATE TABLE payment
 (
-    id BIGINT UNIQUE AUTO_INCREMENT NOT NULL,
-    type_id BIGINT,
-    amount DECIMAL,
+    id BIGINT AUTO_INCREMENT NOT NULL,
+    type_id BIGINT NOT NULL,
+    amount DECIMAL NOT NULL,
     payment_date DATETIME,
-    student_id BIGINT,
+    student_id BIGINT NOT NULL,
     PRIMARY KEY(id)
 );
 ALTER TABLE Payment ADD CONSTRAINT fk_payment_type FOREIGN KEY (type_id) REFERENCES paymentType(id);
