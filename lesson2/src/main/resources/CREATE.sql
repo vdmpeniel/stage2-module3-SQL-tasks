35a6e350fe9dddd747e8c71d0f3c8ed42052488c
victor diaz <vdmpeniel@yahoo.es>
2023-08-15 18:01:07 -0400

    Working on CREATE issues.

--- lesson2/src/main/resources/CREATE.sql
+++ lesson2/src/main/resources/CREATE.sql
@@ -1,15 +1,15 @@
 
 CREATE TABLE student
 (
-    id BIGINT NOT NULL AUTO_INCREMENT,
+    id BIGINT UNIQUE AUTO_INCREMENT NOT NULL,
     name VARCHAR(255),
-    birthday DATE NOT NULL,
-    groupnumber INT NOT NULL,
+    birthday DATE,
+    groupnumber INT,
     PRIMARY KEY(id)
 );
 CREATE TABLE subject
 (
-    id BIGINT NOT NULL AUTO_INCREMENT,
+    id BIGINT UNIQUE AUTO_INCREMENT NOT NULL,
     name VARCHAR(255),
     description VARCHAR(255),
     grade INT NOT NULL,
@@ -18,14 +18,11 @@ CREATE TABLE subject
 
 CREATE TABLE mark
 (
-    id BIGINT NOT NULL AUTO_INCREMENT,
+    id BIGINT UNIQUE AUTO_INCREMENT NOT NULL,
     student_id BIGINT,
     subject_id BIGINT,
-    mark INT NOT NULL,
+    mark INT,
     PRIMARY KEY(id)
-
-    --foreign key (student_id) references student(id),
-    --foreign key (subject_id) references subject(id)
 );
 ALTER TABLE mark ADD CONSTRAINT fk_mark_student FOREIGN KEY (student_id) REFERENCES student(id);
 ALTER TABLE mark ADD CONSTRAINT fk_mark_subject FOREIGN KEY (subject_id) REFERENCES subject(id);
@@ -35,24 +32,20 @@ ALTER TABLE mark ADD CONSTRAINT fk_mark_subject FOREIGN KEY (subject_id) REFEREN
 
 CREATE TABLE paymenttype
 (
-    id BIGINT NOT NULL AUTO_INCREMENT,
+    id BIGINT UNIQUE AUTO_INCREMENT NOT NULL,
     name VARCHAR(255),
     PRIMARY KEY(id)
 );
 
 CREATE TABLE payment
 (
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    type_id BIGINT NOT NULL,
-    amount DECIMAL NOT NULL,
-    payment_date TIMESTAMP  NOT NULL,
-    student_id BIGINT NOT NULL,
+    id BIGINT UNIQUE AUTO_INCREMENT NOT NULL,
+    type_id BIGINT,
+    amount DECIMAL,
+    payment_date DATETIME,
+    student_id BIGINT,
     PRIMARY KEY(id)
-
-    --foreign key (type_id) references paymenttype(id),
-    --foreign key (student_id) references student(id)
 );
-
 ALTER TABLE Payment ADD CONSTRAINT fk_payment_type FOREIGN KEY (type_id) REFERENCES paymentType(id);
 ALTER TABLE Payment ADD CONSTRAINT fk_payment_student FOREIGN KEY (student_id) REFERENCES student(id);
 
