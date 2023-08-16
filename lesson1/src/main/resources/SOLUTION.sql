afa6bbc8e0d59355c8605d8eed908802fafc8f73
victor diaz <vdmpeniel@yahoo.es>
2023-08-16 12:40:27 -0400

    Creating and altering/deleting tables lesson 1 and 12 - #2

--- lesson1/src/main/resources/SOLUTION.sql
+++ lesson1/src/main/resources/SOLUTION.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Student(
-    id BIGINT NOT NULL UNIQUE AUTO_INCREMENT,
+    id BIGINT NOT NULL AUTO_INCREMENT,
     name VARCHAR NOT NULL,
     birthday DATE NOT NULL,
     groupnumber INT NOT NULL,
@@ -7,7 +7,7 @@ CREATE TABLE Student(
 );
 
 CREATE TABLE Subject(
-    id BIGINT NOT NULL UNIQUE AUTO_INCREMENT,
+    id BIGINT NOT NULL AUTO_INCREMENT,
     name VARCHAR NOT NULL,
     description VARCHAR,
     grade INT,
@@ -15,24 +15,24 @@ CREATE TABLE Subject(
 );
 
 CREATE TABLE PaymentType(
-    id BIGINT NOT NULL UNIQUE AUTO_INCREMENT,
+    id BIGINT NOT NULL AUTO_INCREMENT,
     name VARCHAR,
-     PRIMARY KEY ( id )
+    PRIMARY KEY ( id )
 );
 
 CREATE TABLE Payment(
-    id BIGINT NOT NULL UNIQUE AUTO_INCREMENT,
+    id BIGINT NOT NULL AUTO_INCREMENT,
     type_id BIGINT,
     amount DECIMAL,
     student_id BIGINT NOT NULL,
-    payment_date DATETIME,
+    payment_date TIMESTAMP,
     PRIMARY KEY ( id )
 );
 ALTER TABLE Payment ADD CONSTRAINT fk_payment_type FOREIGN KEY (type_id) REFERENCES PaymentType(id);
 ALTER TABLE Payment ADD CONSTRAINT fk_payment_student FOREIGN KEY (student_id) REFERENCES Student(id);
 
 CREATE TABLE Mark(
-    id BIGINT NOT NULL UNIQUE AUTO_INCREMENT,
+    id BIGINT NOT NULL AUTO_INCREMENT,
     student_id BIGINT,
     subject_id BIGINT,
     mark INT NOT NULL,
--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -2,7 +2,7 @@ DELETE FROM Student WHERE groupnumber >= 4;
 
 DELETE FROM Student WHERE id IN ( SELECT DISTINCT student_id FROM mark WHERE mark < 4 );
 
-DELETE FROM Payment WHERE type_id IN ( SELECT DISTINCT id FROM PaymentType WHERE name = 'DAILY' );
+SELECT DISTINCT id FROM PaymentType WHERE name = 'DAILY';
 
 DELETE FROM Mark WHERE mark < 7;
 
