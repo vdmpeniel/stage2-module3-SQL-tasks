400b2bc1b620bfc839ae153e3e0e01e237d5e2b4
victor diaz <vdmpeniel@yahoo.es>
2023-08-16 12:57:21 -0400

    2 - Creating and altering/deleting tables lesson 1 and 12

--- lesson1/src/main/resources/SOLUTION.sql
+++ lesson1/src/main/resources/SOLUTION.sql
@@ -1,44 +1,39 @@
-CREATE TABLE Student(
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    name VARCHAR NOT NULL,
+CREATE TABLE student(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
     birthday DATE NOT NULL,
-    groupnumber INT NOT NULL,
-    PRIMARY KEY( id )
+    groupnumber INT NOT NULL
 );
 
-CREATE TABLE Subject(
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    name VARCHAR NOT NULL,
-    description VARCHAR,
-    grade INT,
-    PRIMARY KEY( id )
+CREATE TABLE subject(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    description VARCHAR(255),
+    grade INT NOT NULL
 );
 
-CREATE TABLE PaymentType(
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    name VARCHAR,
-    PRIMARY KEY ( id )
+CREATE TABLE paymentType(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255)
 );
 
-CREATE TABLE Payment(
-    id BIGINT NOT NULL AUTO_INCREMENT,
+CREATE TABLE payment(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     type_id BIGINT,
     amount DECIMAL,
     student_id BIGINT NOT NULL,
-    payment_date TIMESTAMP,
-    PRIMARY KEY ( id )
+    payment_date TIMESTAMP
 );
-ALTER TABLE Payment ADD CONSTRAINT fk_payment_type FOREIGN KEY (type_id) REFERENCES PaymentType(id);
-ALTER TABLE Payment ADD CONSTRAINT fk_payment_student FOREIGN KEY (student_id) REFERENCES Student(id);
+ALTER TABLE payment ADD CONSTRAINT fk_payment_type FOREIGN KEY (type_id) REFERENCES paymentType(id);
+ALTER TABLE payment ADD CONSTRAINT fk_payment_student FOREIGN KEY (student_id) REFERENCES student(id);
 
 CREATE TABLE Mark(
-    id BIGINT NOT NULL AUTO_INCREMENT,
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     student_id BIGINT,
     subject_id BIGINT,
-    mark INT NOT NULL,
-    PRIMARY KEY ( id )
+    mark INT NOT NULL
 );
-ALTER TABLE Mark ADD CONSTRAINT fk_mark_student FOREIGN KEY (student_id) REFERENCES Student(id);
-ALTER TABLE Mark ADD CONSTRAINT fk_mark_subject FOREIGN KEY (subject_id) REFERENCES Subject(id);
+ALTER TABLE mark ADD CONSTRAINT fk_mark_student FOREIGN KEY (student_id) REFERENCES student(id);
+ALTER TABLE mark ADD CONSTRAINT fk_mark_subject FOREIGN KEY (subject_id) REFERENCES subject(id);
 
 
--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
-DELETE FROM Student WHERE groupnumber >= 4;
+DELETE FROM Student WHERE id IN (SELECT student_id FROM Mark WHERE mark > 4);
 
 DELETE FROM Student WHERE id IN ( SELECT DISTINCT student_id FROM mark WHERE mark < 4 );
 
