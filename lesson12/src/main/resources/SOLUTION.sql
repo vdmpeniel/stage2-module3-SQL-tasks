b9fb76aa70f7e1ef835115e144d842d22f1e279c
victor diaz <vdmpeniel@yahoo.es>
2023-08-16 13:31:20 -0400

    2 - Fixing lesson 12

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,8 +1,8 @@
-DELETE FROM Student WHERE id IN (SELECT student_id FROM Mark WHERE mark > 4);
+DELETE FROM Student WHERE id IN (SELECT DISTINCT student_id FROM Mark AS m JOIN subject AS s ON s.id = m.subject_id WHERE grade >= 4);
 
 DELETE FROM Student WHERE id IN ( SELECT DISTINCT student_id FROM mark WHERE mark < 4 );
 
-SELECT DISTINCT id FROM PaymentType WHERE name = 'DAILY';
+DELETE FROM PaymentType WHERE name = 'DAILY';
 
 DELETE FROM Mark WHERE mark < 7;
 
