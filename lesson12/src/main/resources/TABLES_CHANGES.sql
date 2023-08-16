66f242c6d7602c3e00263a577579a7f3bd80790f
victor diaz <vdmpeniel@yahoo.es>
2023-08-16 12:05:14 -0400

    Creating and altering/deleting tables lesson 1 and 12

--- lesson12/src/main/resources/TABLES_CHANGES.sql
+++ lesson12/src/main/resources/TABLES_CHANGES.sql
@@ -2,6 +2,9 @@
 ALTER TABLE Mark DROP CONSTRAINT mark_student_id_fkey;
 ALTER TABLE Mark ADD CONSTRAINT mark_student_id_fkey FOREIGN KEY (student_id) REFERENCES Student(id) ON DELETE CASCADE;
 
+ALTER TABLE Mark DROP CONSTRAINT mark_subject_id_fkey;
+ALTER TABLE Mark ADD CONSTRAINT mark_subject_id_fkey FOREIGN KEY (subject_id) REFERENCES Subject(id) ON DELETE CASCADE;
+
 ALTER TABLE Payment DROP CONSTRAINT payment_student_id_fkey;
 ALTER TABLE Payment ADD CONSTRAINT payment_student_id_fkey FOREIGN KEY (student_id) REFERENCES Student(id) ON DELETE CASCADE;
 
--- lesson2/src/main/resources/CREATE.sql
+++ lesson2/src/main/resources/CREATE.sql
@@ -1,43 +1,52 @@
 
-CREATE TABLE Student(
+CREATE TABLE student
+(
     id BIGINT NOT NULL AUTO_INCREMENT,
-    name VARCHAR NOT NULL,
+    name VARCHAR(45),
     birthday DATE NOT NULL,
     groupnumber INT NOT NULL,
-    PRIMARY KEY( id )
+    PRIMARY KEY(id)
+);
+CREATE TABLE subject
+(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(250),
+    description VARCHAR(255),
+    grade int NOT NULL,
 );
 
-CREATE TABLE Subject(
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    name VARCHAR NOT NULL,
-    description VARCHAR NOT NULL,
-    grade INT NOT NULL,
-    PRIMARY KEY( id )
+CREATE TABLE mark
+(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    student_id BIGINT,
+    subject_id BIGINT,
+    mark INT NOT NULLgit branch,
+    --foreign key (student_id) references student(id),
+    --foreign key (subject_id) references subject(id)
+);
+ALTER TABLE Mark ADD CONSTRAINT fk_mark_student FOREIGN KEY (student_id) REFERENCES Student(id);
+ALTER TABLE Mark ADD CONSTRAINT fk_mark_subject FOREIGN KEY (subject_id) REFERENCES Subject(id);
+
+
+
+
+CREATE TABLE paymenttype
+(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(45)
 );
 
-CREATE TABLE PaymentType(
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    name VARCHAR NOT NULL,
-    PRIMARY KEY( id )
-);
-
-CREATE TABLE Payment(
-    id BIGINT NOT NULL AUTO_INCREMENT,
+CREATE TABLE payment
+(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     type_id BIGINT NOT NULL,
-    amount DECIMAL NOT NULL,
+    amount decimal NOT NULL,
+    payment_date TIMESTAMP  NOT NULL,
     student_id BIGINT NOT NULL,
-    payment_date TIMESTAMP NOT NULL,
-    PRIMARY KEY( id )
+    --foreign key (type_id) references paymenttype(id),
+    --foreign key (student_id) references student(id)
 );
-ALTER TABLE Payment ADD CONSTRAINT PaymentStudentFk FOREIGN KEY (student_id) REFERENCES Student(id);
-ALTER TABLE Payment ADD CONSTRAINT PaymentPaymentTypeFk FOREIGN KEY (type_id) REFERENCES PaymentType(id);
 
-CREATE TABLE Mark(
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    student_id BIGINT NOT NULL,
-    subject_id BIGINT NOT NULL,
-    mark INT NOT NULL,
-    PRIMARY KEY( id )
-);
-ALTER TABLE Mark ADD CONSTRAINT MarkStudentFk FOREIGN KEY (student_id) REFERENCES Student(id);
-ALTER TABLE Mark ADD CONSTRAINT MarkSubjectFk FOREIGN KEY (subject_id) REFERENCES Subject(id);
+ALTER TABLE Payment ADD CONSTRAINT fk_payment_type FOREIGN KEY (type_id) REFERENCES PaymentType(id);
+ALTER TABLE Payment ADD CONSTRAINT fk_payment_student FOREIGN KEY (student_id) REFERENCES Student(id);
+
